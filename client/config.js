62e12b6a5941837fa6c756671320c5b9538ee582
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2021-12-13 14:04:56 -0500

    fixed back end URI

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-export const endpoint = `https://rocky-beyond-96642.herokuapp.com/`;
+export const endpoint = `https://rocky-plains-31371.herokuapp.com/`;
 export const prodEndpoint = `http://localhost:3000/api/graphql`;
