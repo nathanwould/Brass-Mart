6a51590cc8bac9514b245c5bd70cbe5f54160414
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-01-12 13:34:30 -0500

    changed placeholder accessories page

--- client/pages/accessories.js
+++ client/pages/accessories.js
@@ -1,5 +1,17 @@
+import { Breadcrumb, Layout, Typography } from "antd";
+
 export default function AccessoriesPage() {
+  const { Content } = Layout;
+  const { Title } = Typography;
   return (
-    <p>Howdy!</p>
+    <Layout style={{height: "100%"}}>
+      <Breadcrumb style={{ margin: '1rem 0 0 1.5rem' }}>
+        <Breadcrumb.Item><a href="/">Home</a></Breadcrumb.Item>
+        <Breadcrumb.Item>Accessories</Breadcrumb.Item>
+      </Breadcrumb>
+      <Content className="site-layout-background">
+        <Title>Coming Soon!</Title>
+      </Content>
+    </Layout>
   );
 }
