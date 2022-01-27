31d88288e6a235c160645d28c61756cd4d80d604
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-01-27 11:33:00 -0500

    fixed new db route

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-export const endpoint = `https://brass-mart-db.herokuapp.com/`;
+export const endpoint = `https://brass-mart-db.herokuapp.com`;
 export const prodEndpoint = `http://localhost:3000/api/graphql`;
